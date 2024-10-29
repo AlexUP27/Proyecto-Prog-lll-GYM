cf398de4814c64170b98e94980e890dea1d28de8
AlexUP27 <alex.u.p@opendeusto.es>
2024-10-29 11:57:52 +0100

    Clase reserva y clases terminadas

--- src/bin/Clases.java
+++ src/bin/Clases.java
@@ -1,11 +1,61 @@
 package bin;
 
 public class Clases {
+	private String nombre;
 	private TipoClases tipo;
+	private int capcidadmax;
+	private Monitor monitor;
 
 	public Clases() {
 		super();
 	}
 
+	public Clases(String nombre, TipoClases tipo, int capcidadmax, Monitor monitor) {
+		super();
+		this.nombre = nombre;
+		this.tipo = tipo;
+		this.capcidadmax = capcidadmax;
+		this.monitor = monitor;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public TipoClases getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoClases tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getCapcidadmax() {
+		return capcidadmax;
+	}
+
+	public void setCapcidadmax(int capcidadmax) {
+		this.capcidadmax = capcidadmax;
+	}
+
+	public Monitor getMonitor() {
+		return monitor;
+	}
+
+	public void setMonitor(Monitor monitor) {
+		this.monitor = monitor;
+	}
+
+	@Override
+	public String toString() {
+		return "Clases [nombre=" + nombre + ", tipo=" + tipo + ", capcidadmax=" + capcidadmax + ", monitor=" + monitor
+				+ "]";
+	}
+	
+	
 	
 }
--- src/bin/Reserva.java
+++ src/bin/Reserva.java
@@ -1,5 +1,41 @@
 package bin;
 
 public class Reserva {
+	private Clases clase;
+	private Usuario usuario;
+	
+	public Reserva() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Reserva(Clases clase, Usuario usuario) {
+		super();
+		this.clase = clase;
+		this.usuario = usuario;
+	}
+
+	public Clases getClase() {
+		return clase;
+	}
+
+	public void setClase(Clases clase) {
+		this.clase = clase;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	@Override
+	public String toString() {
+		return "Reserva [clase=" + clase + ", usuario=" + usuario + "]";
+	}
+	
+	
 	
 }
