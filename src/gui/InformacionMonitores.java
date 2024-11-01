c3d7b987651149d477714d43f32e217dbb4392f0
Joseba Marijuan <joseba.marijuan@opendeusto.es>
2024-11-01 17:53:18 +0100

    He actualizado la lista de monitores

--- src/gui/InformacionMonitores.java
+++ src/gui/InformacionMonitores.java
@@ -25,12 +25,13 @@ public class InformacionMonitores extends JFrame{
 		setSize(400,300);
 		
 		List<Monitor> monitores = List.of(
-				new Monitor(111, "Jorge", "Garcia", "Jorge.Garcia@gmail.com", 28, "", 673984609),
-				new Monitor(112, "Irene", "Sanchez", "Irene.Sanchez@gmail.com", 35, "", 639401843),
-				new Monitor(113, "David", "Alvarez", "David.Alvarez@gmail.com", 24, "", 673928356),
-				new Monitor(114, "Daniel", "Hernandez", "Daniel.Hernandez@gmail.com", 42, "", 678549807),
-				new Monitor(115, "Raul", "Martinez", "Raul.Martinez@gmail.com", 31, "", 678643789),
-				new Monitor(116, "Laura", "Ruiz", "Laura.Ruiz@gmail.com", 26, "", 654646746)
+				new Monitor(111, "Jorge", "Garcia", "Jorge.Garcia@gmail.com", 28, "Yoga", 673984609),
+				new Monitor(112, "Irene", "Sanchez", "Irene.Sanchez@gmail.com", 35, "Pilates", 639401843),
+				new Monitor(113, "David", "Alvarez", "David.Alvarez@gmail.com", 24, "Spinning", 673928356),
+				new Monitor(114, "Daniel", "Hernandez", "Daniel.Hernandez@gmail.com", 42, "Boxeo", 678549807),
+				new Monitor(115, "Raul", "Martinez", "Raul.Martinez@gmail.com", 31, "Zumba", 678643789),
+				new Monitor(116, "Laura", "Ruiz", "Laura.Ruiz@gmail.com", 26, "HIIT", 654646746),
+				new Monitor(116, "Iker", "Ruiz", "Iker.Ruiz@gmail.com", 27, "Crossfit", 694846726)
 				);
 		
 		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
