00075aad4e0413fdbe080c4f1e6cf4425d914af3
Joseba Marijuan <joseba.marijuan@opendeusto.es>
2024-12-01 18:43:57 +0100

    En la base de datos he puesto la tabla de clientes y monitores y en el inicio de sesion del monitor para cerrar la ventan lo he cambiado a dispose on close y he añadido el boton de atras

--- src/gui/InicioSesionMonitor.java
+++ src/gui/InicioSesionMonitor.java
@@ -11,12 +11,13 @@ public class InicioSesionMonitor extends JFrame {
     private JPasswordField txtContraseniaUsuario;
     private JButton btnLogin;
     private JLabel lblError;
+    private JButton btnAtras;
 
     public InicioSesionMonitor() {
         setTitle("Inicio de Sesión Monitor");
         setSize(400, 300);
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setResizable(false);
 
         // Panel principal
@@ -38,6 +39,9 @@ public class InicioSesionMonitor extends JFrame {
         panelFormulario.setLayout(new GridLayout(4, 2, 10, 10));
         panelFormulario.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         panelFormulario.setBackground(new Color(245, 245, 245));
+        
+        JPanel panelBotones = new JPanel();
+        panelBotones.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
 
         // Componentes del formulario
         JLabel lblNombreUsuario = new JLabel("Nombre de Usuario:");
@@ -58,6 +62,12 @@ public class InicioSesionMonitor extends JFrame {
         lblError = new JLabel();
         lblError.setForeground(Color.RED);
         lblError.setFont(new Font("Arial", Font.ITALIC, 12));
+        
+        btnAtras = new JButton("Atras");
+        btnAtras.setFont(new Font("Arial", Font.PLAIN, 16));
+        btnAtras.setBackground(new Color(85, 107, 47));
+        btnAtras.setForeground(Color.WHITE);
+        btnAtras.setFocusPainted(false);
 
         // Añadir los componentes al panel de formulario
         panelFormulario.add(lblNombreUsuario);
@@ -66,11 +76,12 @@ public class InicioSesionMonitor extends JFrame {
         panelFormulario.add(txtContraseniaUsuario);
         panelFormulario.add(new JLabel());  // Fila vacía para el espaciado
         panelFormulario.add(lblError);
-        panelFormulario.add(new JLabel());  // Fila vacía
-        panelFormulario.add(btnLogin);
+        panelBotones.add(btnLogin);
+        panelBotones.add(btnAtras);
 
         // Añadir el panel de formulario al panel principal
         panel.add(panelFormulario, BorderLayout.CENTER);
+        panel.add(panelBotones, BorderLayout.SOUTH);
 
         // Acción del botón de login
         btnLogin.addActionListener(new ActionListener() {
@@ -92,6 +103,10 @@ public class InicioSesionMonitor extends JFrame {
                     lblError.setText("Por favor, complete todos los campos.");
                 }
             }
+        });
+        
+        btnAtras.addActionListener(e -> {
+        	dispose();
         });
 
         // Hacer visible la ventana
