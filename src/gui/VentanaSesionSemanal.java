b5bf69891f9641f52d991107bb75631db1b52ee1
jukossound <jon.urkidi@opendeusto.es>
2025-01-06 18:27:02 +0100

    Terminada la funcionalidad del entrenamiento aleatorio

--- src/gui/VentanaSesionSemanal.java
+++ src/gui/VentanaSesionSemanal.java
@@ -1,10 +1,16 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,104 +19,137 @@ public class VentanaSesionSemanal extends JFrame {
 
     private static final long serialVersionUID = 1L;
 
-    public VentanaSesionSemanal() {
-        setTitle("Progreso de Clientes");
-        setBounds(300, 200, 1280, 500); //1280 porque cada foto mide 256x256 y para que entren justas
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    public List<String> entrenamientos = Arrays.asList("Yoga","Pilates","Spinning","Body Pump","HIIT","Power Yoga",
+														"Cardio","TRX","Zumba","Boxeo","Crossfit", "Stretching") ;
+    private boolean running = true;
+    private final Random random = new Random();
 
         
-        List<String> sesionSemanal = generarSesionAleatoria();
+    public VentanaSesionSemanal() {
+        setTitle("Progreso de Clientes");
+        setBounds(300, 200, 1280, 550); 
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLayout(new BorderLayout()); 
 
-        // Configuración del GridLayout
-        JPanel panel = new JPanel(new GridLayout(1, 5));
+        JPanel panelEntrenamientos = new JPanel(new GridLayout(1, 5));
+        
+        JPanel panelLunes = new JPanel(new GridLayout(2, 1));
+	        JLabel imagenLunes = new JLabel();
+	        
+	        panelLunes.add(imagenLunes);
+	        JLabel nombreLunes = new JLabel();
+	        nombreLunes.setHorizontalAlignment(JLabel.CENTER);
+	        panelLunes.add(nombreLunes);
+        
+	        
+        JPanel panelMartes = new JPanel(new GridLayout(2, 1));
+	        JLabel imagenMartes = new JLabel();
+	        
+	        panelMartes.add(imagenMartes);
+	        JLabel nombreMartes = new JLabel();
+	        nombreMartes.setHorizontalAlignment(JLabel.CENTER);
+	        panelMartes.add(nombreMartes);
+        
+	        
+        JPanel panelMiercoles = new JPanel(new GridLayout(2, 1));
+	        JLabel imagenMiercoles = new JLabel();
+	        
+	        panelMiercoles.add(imagenMiercoles);
+	        JLabel nombreMiercoles  = new JLabel();
+	        nombreMiercoles.setHorizontalAlignment(JLabel.CENTER);
+	        panelMiercoles.add(nombreMiercoles);
+        
+	        
+        JPanel panelJueves = new JPanel(new GridLayout(2, 1));
+	        JLabel imagenJueves = new JLabel();
+	        
+	        panelJueves.add(imagenJueves);
+	        JLabel nombreJueves = new JLabel();
+	        nombreJueves.setHorizontalAlignment(JLabel.CENTER);
+	        panelJueves.add(nombreJueves);
+        
+	        
+        JPanel panelViernes = new JPanel(new GridLayout(2, 1));
+	        JLabel imagenViernes = new JLabel();
+	        
+	        panelViernes.add(imagenViernes);
+	        JLabel nombreViernes = new JLabel();
+	        nombreViernes.setHorizontalAlignment(JLabel.CENTER);
+	        panelViernes.add(nombreViernes);
+        
+	    panelEntrenamientos.add(panelLunes);
+	    panelEntrenamientos.add(panelMartes);
+	    panelEntrenamientos.add(panelMiercoles);
+	    panelEntrenamientos.add(panelJueves);
+	    panelEntrenamientos.add(panelViernes);
+        
+	    JPanel panelBoton = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JButton boton = new JButton("CREAR RUTINA");
+        boton.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 14));
+        boton.setBackground(Color.RED);
+        boton.setForeground(Color.WHITE);
+        
+        JButton boton2 = new JButton("GUARDAR RUTINA");
+        boton2.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 14));
+        boton2.setBackground(Color.GREEN);
+        boton2.setForeground(Color.WHITE);
+        
+        panelBoton.add(boton);
+        panelBoton.add(boton2);
 
-        // Añadir entrenamientos al panel
-        int dia = 0;
-        for (String entrenamiento : sesionSemanal) {
-            JPanel entrenamientosPanel = new JPanel(new GridLayout(2, 1));
-
-            // Añadir imagen del entrenamiento
-            JLabel imagen = new JLabel();
-            ImageIcon icono = new ImageIcon("img/" + entrenamiento + ".png");
-            imagen.setIcon(icono);
-            imagen.setHorizontalAlignment(JLabel.CENTER);
-            entrenamientosPanel.add(imagen);
-            
-            if (dia == 0) {
-                JLabel nombreLabel = new JLabel("Lunes:"+entrenamiento);
-                nombreLabel.setHorizontalAlignment(JLabel.CENTER);
-                entrenamientosPanel.add(nombreLabel);
-
-                panel.add(entrenamientosPanel);
+        add(panelEntrenamientos, BorderLayout.CENTER);
+        add(panelBoton, BorderLayout.SOUTH);
+        
+        
+        Thread hilo = new Thread(() -> {
+			
+        		while (running) {
+    			
+        			List<String> randomEntrenamientos = new ArrayList<>();               
+        			for (int i = 0; i < 5; i++) {
+                    randomEntrenamientos.add(entrenamientos.get(random.nextInt(entrenamientos.size())));
+        			}
+        			
+        			ImageIcon iconoLunes = new ImageIcon("img/" + randomEntrenamientos.get(0) + ".png");
+                    imagenLunes.setIcon(iconoLunes);
+                    nombreLunes.setText("Lunes: " +randomEntrenamientos.get(0));
+                    ImageIcon iconoMartes = new ImageIcon("img/" + randomEntrenamientos.get(1) + ".png");
+                    imagenMartes.setIcon(iconoMartes);
+                    nombreMartes.setText("Martes: " +randomEntrenamientos.get(1));
+                    ImageIcon iconoMiercoles = new ImageIcon("img/" + randomEntrenamientos.get(2) + ".png");
+                    imagenMiercoles.setIcon(iconoMiercoles);
+                    nombreMiercoles.setText("Miercoles: " +randomEntrenamientos.get(2));                
+                    ImageIcon iconoJueves = new ImageIcon("img/" + randomEntrenamientos.get(3) + ".png");
+                    imagenJueves.setIcon(iconoJueves);
+                    nombreJueves.setText("Jueves: " +randomEntrenamientos.get(3));
+                    ImageIcon iconoViernes = new ImageIcon("img/" + randomEntrenamientos.get(4) + ".png");
+                    imagenViernes.setIcon(iconoViernes);
+                    nombreViernes.setText("Viernes: " +randomEntrenamientos.get(4));
+        			
+				
+                    
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
                 }
-            else if(dia == 1) {
-                JLabel nombreLabel = new JLabel("Martes:"+entrenamiento);
-                nombreLabel.setHorizontalAlignment(JLabel.CENTER);
-                entrenamientosPanel.add(nombreLabel);
+                
+            }
+        });
 
-                panel.add(entrenamientosPanel);
-                }
-            else if (dia == 2) {
-                JLabel nombreLabel = new JLabel("Miercoles:"+entrenamiento);
-                nombreLabel.setHorizontalAlignment(JLabel.CENTER);
-                entrenamientosPanel.add(nombreLabel);
-
-                panel.add(entrenamientosPanel);
-                }
-            else if (dia == 3) {
-                JLabel nombreLabel = new JLabel("Jueves:"+entrenamiento);
-                nombreLabel.setHorizontalAlignment(JLabel.CENTER);
-                entrenamientosPanel.add(nombreLabel);
-
-                panel.add(entrenamientosPanel);
-                }
-            else {
-                JLabel nombreLabel = new JLabel("Viernes:"+entrenamiento);
-                nombreLabel.setHorizontalAlignment(JLabel.CENTER);
-                entrenamientosPanel.add(nombreLabel);
-
-                panel.add(entrenamientosPanel);
-                }
-            dia++;
-        }
-
-        add(panel);
-        setVisible(true);
+        hilo.start();
+        
+        
+        boton.addActionListener(e -> {
+        	hilo.interrupt();
+        });
+        
+        boton2.addActionListener( e -> {
+        	//Guardar en la BD la informacion de la rutina asociada al cliente
+        });
+        
+        setVisible(true);    
+ 
     }
-
-    public List<String> generarSesionSemanal(List<List<String>> entrenamientos) {
-        return generarSesionRecursiva(entrenamientos, entrenamientos.size() - 1, new ArrayList<>());
-    }
-
-    private List<String> generarSesionRecursiva(List<List<String>> entrenamientos, int diasRestantes, List<String> sesionActual) {
-        if (diasRestantes == 0) {
-            return sesionActual;
-        }
-
-        List<String> diaActualEntrenamientos = entrenamientos.get(entrenamientos.size() - diasRestantes);
-        String entrenamientoAleatorio = diaActualEntrenamientos.get(new Random().nextInt(diaActualEntrenamientos.size()));
-
-        sesionActual.add(entrenamientoAleatorio);
-
-        return generarSesionRecursiva(entrenamientos, diasRestantes - 1, sesionActual);
-    }
-
-    public List<List<String>> generarEntrenamientos() {
-        List<List<String>> entrenamientosSemanales = new ArrayList<>();
-        entrenamientosSemanales.add(List.of("Yoga", "Pilates", "Spinning", "Body Pump", "HIIT"));
-        entrenamientosSemanales.add(List.of("Cardio", "Yoga", "TRX", "Zumba", "Boxeo"));
-        entrenamientosSemanales.add(List.of("Crossfit", "Body Pump", "Cardio", "Crossfit", "Stretching"));
-        entrenamientosSemanales.add(List.of("Zumba", "HIIT", "Power Yoga", "Body Pump", "Spinning"));
-        entrenamientosSemanales.add(List.of("Spinning", "Crossfit", "TRX", "HIIT", "Body Pump"));
-        entrenamientosSemanales.add(List.of("Boxeo", "Zumba", "Stretching", "Pilates", "Cardio"));
-
-        return entrenamientosSemanales;
-    }
-
-    public List<String> generarSesionAleatoria() {
-        List<List<String>> entrenamientosSemanales = generarEntrenamientos();
-        return generarSesionSemanal(entrenamientosSemanales);
-    }
-    
 }
